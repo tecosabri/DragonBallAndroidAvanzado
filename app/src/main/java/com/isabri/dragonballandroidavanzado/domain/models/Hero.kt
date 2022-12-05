f747b608f8bdfbe8d52949f6667c30844c83ae90
Ismael Sabri <hisme14@gmail.com>
2022-12-05 14:09:18 +0100

    Updated Hero model to match API response

--- app/src/main/java/com/isabri/dragonballandroidavanzado/domain/models/Hero.kt
+++ app/src/main/java/com/isabri/dragonballandroidavanzado/domain/models/Hero.kt
@@ -1,6 +1,11 @@
 package com.isabri.dragonballandroidavanzado.domain.models
 
+import java.lang.ref.PhantomReference
+
 data class Hero(
     val id: String,
-    val name: String
+    val name: String,
+    val photo: String,
+    val description: String,
+    val favorite: String
 )
