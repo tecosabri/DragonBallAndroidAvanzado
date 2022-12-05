d3e0ad2deb4faf0983b41742d5740a09b355bf42
Ismael Sabri <hisme14@gmail.com>
2022-12-05 14:36:45 +0100

    Added Moshi library to parse JSON

--- app/src/main/java/com/isabri/dragonballandroidavanzado/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/isabri/dragonballandroidavanzado/data/remote/RemoteDataSource.kt
@@ -1,12 +1,19 @@
 package com.isabri.dragonballandroidavanzado.data.remote
 
 import com.isabri.dragonballandroidavanzado.domain.models.Hero
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 
 class RemoteDataSource {
-
+    private var moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
     private var retrofit = Retrofit.Builder()
         .baseUrl("https://vapor2022.herokuapp.com")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
     private var api: DragonBallAPI = retrofit.create(DragonBallAPI::class.java)
 
