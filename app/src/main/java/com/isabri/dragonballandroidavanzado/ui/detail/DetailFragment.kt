b88284d29cfb92709f2f40c475401f01ddcd5c17
Ismael Sabri <hisme14@gmail.com>
2022-12-15 11:12:16 +0100

    Set up google maps in hero detail fragment

--- app/src/main/java/com/isabri/dragonballandroidavanzado/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/isabri/dragonballandroidavanzado/ui/detail/DetailFragment.kt
@@ -7,21 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import coil.load
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.isabri.dragonballandroidavanzado.R
 import com.isabri.dragonballandroidavanzado.databinding.FragmentDetailBinding
 import com.isabri.dragonballandroidavanzado.ui.heroesList.HeroesListState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DetailFragment : Fragment() {
+class DetailFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
     private val detailViewModel: DetailViewModel by viewModels()
     private val args: DetailFragmentArgs by navArgs()
+    private lateinit var map: GoogleMap
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +42,7 @@ class DetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         detailViewModel.getHeroes(args.hero.name)
         observeHeroesListState()
+        setUpMap()
     }
 
     private fun observeHeroesListState() {
@@ -43,15 +51,29 @@ class DetailFragment : Fragment() {
                 is HeroesListState.Failure -> Toast.makeText(requireContext(), heroesListState.error, Toast.LENGTH_SHORT).show()
                 is HeroesListState.Success -> {
                     val hero = heroesListState.heroes.first()
-                    binding.heroPhoto.load(hero.photo)
                     binding.heroName.text = hero.name
                 }
             }
         }
     }
 
+    private fun setUpMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(
+            MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
